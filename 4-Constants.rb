b4654ea51a41371f961f179760cb22bf925bafb2
sansuaza <santi11_sz@hotmail.com>
2021-02-17 15:54:02 -0500

    modiffied in branch chapter 4

--- 4-Constants.rb
+++ 4-Constants.rb
@@ -1,4 +1,5 @@
  # Tecnicamente ruby permita cambiar el valor de una constante, pero
  # en lo posible no se debería hacer, es una mala practica
  # Se declaran en mayuscula
+	 # Modificado en Chapter4
  Name = "Pepe"
