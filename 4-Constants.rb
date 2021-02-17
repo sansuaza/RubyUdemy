0103ff8dc107927c0d2e12de3d5f9a49455094cd
sansuaza <santi11_sz@hotmail.com>
2021-02-17 15:52:04 -0500

    modification

--- 4-Constants.rb
+++ 4-Constants.rb
@@ -1,4 +1,5 @@
  # Tecnicamente ruby permita cambiar el valor de una constante, pero
  # en lo posible no se debería hacer, es una mala practica
  # Se declaran en mayuscula
+ # Cambio numero 1
  Name = "Pepe"
