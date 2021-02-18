3921e78fc702ea3a0fce9b8c5109025dbbfda4b3
sansuaza <santi11_sz@hotmail.com>
2021-02-18 10:59:38 -0500

    Finished the section 3 of the course

--- 5-Metodos.rb
+++ 5-Metodos.rb
@@ -3,6 +3,10 @@
 
 
 #INTEGER
+3.between?(1 , 5) # true
+
+
+
 # convertir numero en string
 p 5.to_s
 
@@ -16,5 +20,13 @@ p 10.odd? #true
 p 10.even?
 
 #Interpolacion de variables 
+
+#--------------------------------------------------------
+#FLOAT
+p 10.9.to_i #redondea a 10
+p 10.5.floor #redondea abajo
+p 10.5.ceil #redondea arriba  
+p 10.6.round #redondea de manera logica
+p -10.abs #valor absoluto
 name = pepe
 p "hello #{name}"
---
+++ section3/Step.rb
@@ -0,0 +1,2 @@
+# En un rango, va aumentando cierta cantidad de numeros
+0.step(100, 5) { |number| puts number}
