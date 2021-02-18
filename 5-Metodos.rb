6b5a3961e44b94b1b75e2353503b014b0127fcf1
sansuaza <santi11_sz@hotmail.com>
2021-02-18 08:43:48 -0500

    added all files to do push

---
+++ 5-Metodos.rb
@@ -0,0 +1,20 @@
+#STRING
+"string".to_i
+
+
+#INTEGER
+# convertir numero en string
+p 5.to_s
+
+#devuelve el siguiente valor del numero
+p 10.next
+
+#preguntar si es impar
+p 10.odd? #true
+
+# Preguntar si es par
+p 10.even?
+
+#Interpolacion de variables 
+name = pepe
+p "hello #{name}"
---
+++ 6-gets_method.rb
@@ -0,0 +1,5 @@
+puts "cual es tu nombre?"
+# Es como el syso input en java
+name = gets.chomp
+
+puts name
