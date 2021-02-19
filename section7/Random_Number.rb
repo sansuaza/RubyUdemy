45ae85f00598f879fc4315a3dbb56fd5c9cd5353
sansuaza <santi11_sz@hotmail.com>
2021-02-19 16:13:53 -0500

    Finished the section of the course

--- section7/Random_Number.rb
+++ section7/Random_Number.rb
@@ -1,1 +1,4 @@
 puts rand.round(4) #Cuatro numeros decimales
+puts rand(100) #Numeros entre 0 y 100
+puts rand * 100 #Como se hace en java conservando los decimales
+puts rand(50..60) # se pone tope superior e inferior
---
+++ section8/Spaceship_Operator.rb
@@ -0,0 +1,10 @@
+# Se comparan dos elementos si l la der es mayor, retorna -1
+# de lo contrario 1, si son iguales 0
+# Con arreglos los compara todos
+
+irb(main):009:0> 4 <=> 5
+=> -1
+irb(main):010:0> 4 <=> 3
+=> 1
+irb(main):011:0> 4 <=> 4
+=> 0
